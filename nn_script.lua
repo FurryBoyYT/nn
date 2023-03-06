b6ab8b2c991be6740add92f0590eef814be0bc95
FurryBoy <104305700+FurryBoyYT@users.noreply.github.com>
2023-03-06 17:53:44 +0200

    Update nn_script.lua

--- nn_script.lua
+++ nn_script.lua
@@ -1,80 +1,52 @@
-local coreGui = Instance.new("ScreenGui", game.CoreGui)
-coreGui.Name = "AAAAAA"
+--[[
+Not obfuscated because I don't care and it's pointless because you can literally find most of this in like 2 minutes, and hey maybe you can learn how to make toggles or new scripts with this :wink:
+Works best with the rdite one Idk about the others
 
-local textButtonSong1 = Instance.new("TextButton", coreGui)
-textButtonSong1.TextSize = "15"
-textButtonSong1.Text = "Play Song 1\nCootie"
-textButtonSong1.Name = "song1Button"
-textButtonSong1.Size = UDim2.new(0, 150, 0, 50)
-textButtonSong1.Position = UDim2.new(0, 1675, 0, 300)
+btw if it not working fully ur executor is probably just shit because this game never gets updated lmao
+]]--
 
-local textButtonSong2 = Instance.new("TextButton", coreGui)
-textButtonSong2.TextSize = "15"
-textButtonSong2.Text = "Play Song 2\nPOSSESSION"
-textButtonSong2.Name = "song2Button"
-textButtonSong2.Size = UDim2.new(0, 150, 0, 50)
-textButtonSong2.Position = UDim2.new(0, 1675, 0, 355)
+print("ok cart game troll GUI loaded lmao") --remove if you want
 
-local textButtonSong3 = Instance.new("TextButton", coreGui)
-textButtonSong3.TextSize = "15"
-textButtonSong3.Text = "Play Song 3\nW SONG"
-textButtonSong3.Name = "song3Button"
-textButtonSong3.Size = UDim2.new(0, 150, 0, 50)
-textButtonSong3.Position = UDim2.new(0, 1675, 0, 410)
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("nico's nextbots", "BloodTheme")
 
-local textButtonSong4 = Instance.new("TextButton", coreGui)
-textButtonSong4.TextSize = "12"
-textButtonSong4.Text = "Play Song 4\nVideo Game Lobby"
-textButtonSong4.Name = "song4Button"
-textButtonSong4.Size = UDim2.new(0, 150, 0, 50)
-textButtonSong4.Position = UDim2.new(0, 1675, 0, 466)
+--TABS
+local MainTAB = Window:NewTab("Songs")
+local ControlsTAB = Window:NewTab("Controls")
+local SettingsTAB = Window:NewTab("Settings")
 
-local textButtonSong5 = Instance.new("TextButton", coreGui)
-textButtonSong5.TextSize = "15"
-textButtonSong5.Text = "Play Song 5\nLoading Theme"
-textButtonSong5.Name = "song5Button"
-textButtonSong5.Size = UDim2.new(0, 150, 0, 50)
-textButtonSong5.Position = UDim2.new(0, 1675, 0, 522)
 
-local textButtonPause = Instance.new("TextButton", coreGui)
-textButtonPause.TextSize = "15"
-textButtonPause.Text = "Pause/Resume"
-textButtonPause.Name = "pauseButton"
-textButtonPause.Size = UDim2.new(0, 150, 0, 50)
-textButtonPause.Position = UDim2.new(0, 1675, 0, 600)
+--SECTIONS INSIDE TABS
+local Songs = MainTAB:NewSection("Songs")
+local Controls = ControlsTAB:NewSection("Controls")
+local Settings = SettingsTAB:NewSection("Settings")
 
-local textButtonRewind = Instance.new("TextButton", coreGui)
-textButtonRewind.TextSize = "20"
-textButtonRewind.Text = "Rewind"
-textButtonRewind.Name = "rewindButton"
-textButtonRewind.Size = UDim2.new(0, 150, 0, 50)
-textButtonRewind.Position = UDim2.new(0, 1675, 0, 655)
 
-textButtonSong1.MouseButton1Click:Connect(function()
+--BUTTONS
+Songs:NewButton("Play: Cootie", "Song name: cootie", function()
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("play", "rbxassetid://7819487317")
 end)
-
-textButtonSong2.MouseButton1Click:Connect(function()
+Songs:NewButton("Play: POSSESSION", "Song name: POSSESSION", function()
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("play", "rbxassetid://11884943198")
 end)
-
-textButtonSong3.MouseButton1Click:Connect(function()
+Songs:NewButton("Play: nico's song", "Song name: what if you're right, and they're wrong?", function()
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("play", "rbxassetid://7016017390")
 end)
-
-textButtonSong4.MouseButton1Click:Connect(function()
+Songs:NewButton("Play: Video Game Lobby", "Song name: video game lobby", function()
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("play", "rbxassetid://12427488975")
 end)
-
-textButtonSong5.MouseButton1Click:Connect(function()
+Songs:NewButton("Play: Loading Theme", "Song name: kensuke", function()
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("play", "rbxassetid://11274394114")
 end)
-
-
-textButtonPause.MouseButton1Click:Connect(function()
+Songs:NewButton("Play: Sus Hub Intro", "Song name: Sus Hub Intro", function()
+    game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("play", "rbxassetid://9091915146")
+end)
+Controls:NewButton("Pause", "Pause", function()
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("pause")
 end)
-
-textButtonRewind.MouseButton1Click:Connect(function()
+Controls:NewButton("Rewind", "Pause", function()
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("rewind")
+end)
+Settings:NewKeybind("Toggle GUI", "Toggles GUI when a key has been pressed", Enum.KeyCode.T, function()
+	Library:ToggleUI()
 end)
