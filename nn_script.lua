e0b2a95f6c706e41951d1a9ff6b224f553bdff83
FurryBoy <104305700+FurryBoyYT@users.noreply.github.com>
2023-03-07 22:23:02 +0200

    Update nn_script.lua

--- nn_script.lua
+++ nn_script.lua
@@ -1,6 +1,3 @@
-getgenv().Theme = "BloodTheme" 
--- Themes: | DarkTheme | LightTheme | BloodTheme | GrapeTheme | Ocean | Midnight | Sentinel | Synapse | Serpent |
-
 --LIBRARY LOAD
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("nico's nextbots", Theme)
@@ -21,43 +18,43 @@ local settings = settingsTab:NewSection("Gui")
 
 
 --SONGS
-songs:NewButton("Cootie", "Song name : cootie", function()
+songs:NewButton("Cootie", "7819487317", function()
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("play", "rbxassetid://7819487317")
 end)
-songs:NewButton("POSSESSION", "Song name : POSSESSION", function()
+songs:NewButton("POSSESSION", "11884943198", function()
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("play", "rbxassetid://11884943198")
 end)
-songs:NewButton("nico's song", "Song name : what if you're right, and they're wrong?", function()
+songs:NewButton("nico's song", "7016017390", function()
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("play", "rbxassetid://7016017390")
 end)
-songs:NewButton("Video Game Lobby", "Song name : video game lobby", function()
+songs:NewButton("Video Game Lobby", "12427488975", function()
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("play", "rbxassetid://12427488975")
 end)
-songs:NewButton("Loading Theme", "Song name : kensuke", function()
+songs:NewButton("Loading Theme", "11274394114", function()
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("play", "rbxassetid://11274394114")
 end)
 
 --ID
-audioId:NewTextBox("Audio ID", "Paste an Audio ID to play!", function(audioId)
+audioId:NewTextBox("Audio ID", "Paste an Audio ID to play.", function(audioId)
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("play", "rbxassetid://" .. audioId)
 end)
 
 --SOUNDS
-sounds:NewButton("Sus Hub Intro", "Sound name : Sus Hub Intro", function()
+sounds:NewButton("Sus Hub Intro", "9091915146", function()
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("play", "rbxassetid://9091915146")
     wait(3)
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("pause")
 end)
 
 --CONTROLS
-controls:NewButton("Pause/Resume", "Pause or resume the songs or looped sounds.", function()
+controls:NewButton("Pause/Resume", "Pause or resume the songs or sounds", function()
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("pause")
 end)
-controls:NewButton("Rewind", "Rewinds the song and looped sounds", function()
+controls:NewButton("Rewind", "Rewinds the song or sounds", function()
     game:GetService("ReplicatedStorage").events.player["local"].boomboxsound:FireServer("rewind")
 end)
 
 --SETTINGS
-settings:NewKeybind("Toggle GUI", "Toggles GUI when a key has been pressed", Enum.KeyCode.RightShift, function()
+settings:NewKeybind("Toggle GUI", "Toggles GUI when a key has been pressed", Enum.KeyCode.T, function()
 	Library:ToggleUI()
 end)
