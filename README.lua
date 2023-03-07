395d4a906d52c6b0e8a37a6acd4cf591e45fcb7b
FurryBoy <104305700+FurryBoyYT@users.noreply.github.com>
2023-03-07 13:01:53 +0200

    Update and rename README.txt to README.lua

---
+++ README.lua
@@ -0,0 +1,4 @@
+getgenv().Theme = "BloodTheme" 
+-- Themes: DarkTheme | LightTheme | BloodTheme | GrapeTheme | Ocean | Midnight | Sentinel | Synapse | Serpent
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/FurryBoyYT/nn/main/nn_main_script.lua'))()
--- README.txt
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/FurryBoyYT/nn/main/nn_main_script.lua'))()
