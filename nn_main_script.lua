3d1a56c9ae5425413a93fb112cd48a615ff995fb
FurryBoy <104305700+FurryBoyYT@users.noreply.github.com>
2023-03-23 04:32:18 +0200

    Update nn_main_script.lua

--- nn_main_script.lua
+++ nn_main_script.lua
@@ -2,7 +2,10 @@ if not game:IsLoaded() then game.Loaded:Wait() end
 local tableFindGamePlace = {
     10118559731,
     12135007762,
-    12629753855
+    12629753855,
+    12629802522,
+    12135006365,
+    12629804027
 }
 if table.find(tableFindGamePlace, game.PlaceId) then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/FurryBoyYT/nn/main/nn_script.lua'))()
